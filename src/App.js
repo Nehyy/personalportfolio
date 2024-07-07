402e99ede04f5cf29bda916cdd69cbace5d9e0f2
Dornala Neha <nehadornala587@gmail.com>
2024-07-07 13:57:02 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from "React";
+
+export default function App() {
+  return (
+    <main>
+      <Navbar />
+      <About />
+      <Projects />
+      <Skills />
+      <Testimonials />
+      <Contact />
+    </main>
+  );
+}
